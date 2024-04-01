3518ecd78ac8602fbe5df3db6cd883a96a4bcb3a
NotCompsky <adammarkgray@gmail.com>
2024-04-01 22:24:22 +0100

    Some minifying

--- files/js/rand.html.js
+++ files/js/rand.html.js
@@ -1,70 +1,57 @@
-const filterfn = [
-	function(fileid){ return true; },
-	function(fileid){ return all_files__as_dict[fileid][3].startsWith("image/"); },
-	function(fileid){ return all_files__as_dict[fileid][3] === "image/gif"; },
-	function(fileid){ console.log(fileid, all_files__as_dict[fileid]); return all_files__as_dict[fileid][3].startsWith("video/"); },
-	function(fileid){ return all_files__as_dict[fileid][3].startsWith("audio/"); },
-	function(fileid){ return all_files__as_dict[fileid][3].startsWith("video/") || all_files__as_dict[fileid][3].startsWith("audio/"); },
-	function(fileid){ return all_files__as_dict[fileid][3].startsWith("video/") || all_files__as_dict[fileid][3]==="image/gif"; }
-];
+const $=document.getElementById.bind(document),
+	global_version=MACRO__GLOBAL_VERSION,
+	b=(a,b)=>a.startsWith(b), // saves 9 characters each use
+	p=(a,b)=>a.push(b), // saves 3 characters each use
+	l=a=>a.length, // used 12 times; saves 4 characters each use
+	e=(a,f)=>a.addEventListener("pointerup",f), // saves 15 chars each use
+	o=(i,s)=>`<option value="${i}">${s}</option>`,
+	filterfn=[
+		function(fileid){ return true; },
+		function(fileid){ return b(all_files__as_dict[fileid][3],"image/"); },
+		function(fileid){ return all_files__as_dict[fileid][3] === "image/gif"; },
+		function(fileid){ console.log(fileid, all_files__as_dict[fileid]); return b(all_files__as_dict[fileid][3],"video/"); },
+		function(fileid){ return b(all_files__as_dict[fileid][3],"audio/"); },
+		function(fileid){ return b(all_files__as_dict[fileid][3],"video/") || b(all_files__as_dict[fileid][3],"audio/"); },
+		function(fileid){ return b(all_files__as_dict[fileid][3],"video/") || all_files__as_dict[fileid][3]==="image/gif"; }
+	];
 
 
-const $ = document.getElementById.bind(document);
-
-
-const global_version = MACRO__GLOBAL_VERSION;
-var all_tags_id2name = null;
-var all_files = null;
-var all_files__as_dict = {};
-var all_tags_id2files = [];
-var tag2thumbnail = null;
-var introduction_to_each_category = null;
-
-var errcontainer = null;
-var container = null;
-var media_video = null;
 //var media_audio = null;
-var media_video_source = null;
 //var media_audio_source = null;
-var media_video_subtitles = null;
-var selector1 = null;
-var selector2 = null;
-var selector3 = null;
-var filterselector = null;
-var nextbtn = null;
-var filetagcontainer = null;
-var shouldloop = 1;
 
 
-let html_indx = 0;
-let prev_html = [""];
-let prev_tag = [0,0,0,0];
-let next_direction = 1;
-let current_media_id = 0;
-let show_file_tags = true; // TODO
-function p(a,b){
-	a.push(b); // saves 3 characters each use
-}
-function l(a){
-	return a.length; // used 12 times; saves 4 characters each use
-}
-function e(a,f){
-	a.addEventListener("pointerup",f); // saves 15 chars each use
-}
-function throw_err(s){
-	errcontainer.innerText = s;
-	errcontainer.style.display = "block";
-	throw Error(s);
-}
+let all_tags_id2name=null,
+	all_files=null,
+	all_files__as_dict={},
+	all_tags_id2files=[],
+	tag2thumbnail=null,
+	introduction_to_each_category=null,
+	errcontainer=null,
+	container=null,
+	media_video=null,
+	media_video_source=null,
+	media_video_subtitles=null,
+	selector1=null,
+	selector2=null,
+	selector3=null,
+	filterselector=null,
+	nextbtn=null,
+	filetagcontainer=null,
+	shouldloop=1,
+	$$$audio_playbackRate=1.0,
+	$$$media_volume=0.0,
+	html_indx=0,
+	prev_html=[""],
+	prev_tag=[0,0,0,0],
+	next_direction=1,
+	current_media_id=0;
+
 function tagbtnclicked(e){
 	selector1.value = (e.currentTarget.dataset.i|0) + 1;
 	get_new_media();
 }
 function shuffle(array, preset_first_n_elements){ // Fisher-Yates (aka Knuth) Shuffle
-	let currentIndex = preset_first_n_elements;
-	let randomIndex;
-	// While there remain elements to shuffle.
-	const N = l(array);
+	let currentIndex=preset_first_n_elements,randomIndex,N=l(array);
 	while(currentIndex < N){
 		// Pick a remaining element.
 		randomIndex = currentIndex + Math.floor(Math.random() * (N-currentIndex));
@@ -77,9 +64,8 @@ function shuffle(array, preset_first_n_elements){ // Fisher-Yates (aka Knuth) Sh
 function render_tags(){
 	for (let i = 0;  i < l(all_files);  ++i){
 		if (all_files[i][0] === current_media_id){
-			const tag_ids = all_files[i][2];
 			let html = "";
-			for (let tagid of tag_ids){
+			for (let tagid of all_files[i][2]){
 				html += `<button data-i="${tagid}">${all_tags_id2name[tagid]}</button>`;
 			}
 			filetagcontainer.innerHTML = html;
@@ -104,13 +90,13 @@ function rendermedia(){
 		ls[1] = "image/jpeg";
 	if (ls[1] === "")
 		ls[1] = "video/webm"; // Fallback
-	if (ls[1].startsWith("image/")){
+	if (b(ls[1],"image/")){
 			container.style.background = `url(${url})`;
 			media_video.style.display = "none";
 			//media_audio.style.display = "none";
 			media_video.pause();
 			//media_audio.pause();
-	/*} else if (ls[1].startsWith("audio/")){
+	/*} else if (b(ls[1],"audio/")){
 			container.style.background = `url(${ls[2]})`;
 			media_audio_source.type = ls[1];
 			media_audio_source.src = url;
@@ -148,7 +134,7 @@ function rendermedia(){
 			
 			media_video_source.setAttribute("type", ls[1]);
 			media_video_source.setAttribute("src",  url);
-			if (ls[1].startsWith("audio/")){
+			if (b(ls[1],"audio/")){
 				const thumbnail = (typeof ls[2] === "number") ? tag2thumbnail[ls[2]] : ls[2];
 				media_video.poster = thumbnail;
 			} else {
@@ -166,9 +152,7 @@ function rendermedia(){
 				console.log(e);
 			}
 	}
-	if (show_file_tags){
-		render_tags();
-	}
+	render_tags();
 }
 function get_new_media(){
 	next_direction = 1;
@@ -205,7 +189,9 @@ function get_new_media(){
 		}
 		shuffle(ls,ls_offset);
 		if (l(ls) === 0){
-			throw_err("0 results");
+			errcontainer.innerText = "0 results";
+			errcontainer.style.display = "block";
+			return;
 		}
 		for (let x of ls){
 			if (x !== prev_html[html_indx]){
@@ -235,8 +221,6 @@ function onmediaend(e){
 	if (!e.currentTarget.loop)
 		get_new_media();
 }
-let $$$audio_playbackRate = 1.0;
-let $$$media_volume = 0.0;
 function $$$media_rate_changed(eventobj){
 	$$$audio_playbackRate = eventobj.target.playbackRate;
 	/* Has to be done at start of each play() event, apparently
@@ -287,23 +271,18 @@ e(media_video,"volumechange",$$$media_volume_changed);
 //e(media_audio,"ratechange",$$$media_rate_changed);
 //e(media_audio,"volumechange",$$$media_volume_changed);
 
-function o(i,s){
-	return `<option value="${i}">${s}</option>`;
-}
-
 fetch("MACRO__ALL_FILES_JSON_PATH", {credentials:"include", mode:"no-cors", method:"GET"}).then(r => {
 	if (!r.ok){
 		showerr(`Server returned ${r.status}: ${r.statusText}`);
 	}
 	r.text().then(datastr => {
 		[all_tags_id2name,all_files,tag2thumbnail,introduction_to_each_category] = JSON.parse(datastr);
-		for (let i = 0;  i < l(all_tags_id2name)+1;  ++i){
+		let s="",h=document.location.hash,optionshtml="",notoptionshtml="",i=0;
+		for (;  i < l(all_tags_id2name)+1;  ++i){
 			p(all_tags_id2files,[]);
 		}
-		let s = "";
-		for (let i = 0;  i < l(all_files);  ++i){
-			const ls = all_files[i];
-			const fileid = ls[0];
+		for (i = 0;  i < l(all_files);  ++i){
+			const ls=all_files[i],fileid=ls[0];
 			all_files__as_dict[fileid] = ls;
 			p(all_tags_id2files[0],fileid);
 			for (let tagid of ls[2])
@@ -311,19 +290,17 @@ fetch("MACRO__ALL_FILES_JSON_PATH", {credentials:"include", mode:"no-cors", meth
 			if (ls[2].length === 0)
 				console.warn("No tagids:", fileid);
 		}
-		let optionshtml = '';
-		let notoptionshtml = '';
-		for (let tagid = 0;  tagid < l(all_tags_id2name);  ++tagid){
-			const tagname = all_tags_id2name[tagid];
-			optionshtml += o(tagid+1,`${tagname} [${all_tags_id2files[tagid+1].length}]`);
-			notoptionshtml += o(tagid+1,`NOT ${tagname}`);
+		for (i = 0;  i < l(all_tags_id2name);  ++i){ // i === tagid
+			const tagname = all_tags_id2name[i];
+			optionshtml += o(i+1,`${tagname} [${all_tags_id2files[i+1].length}]`);
+			notoptionshtml += o(i+1,`NOT ${tagname}`);
 		}
 		selector1.innerHTML = o(0,`Shuffle all [${all_tags_id2files[0].length}]`) + optionshtml;
 		selector2.innerHTML = o(0,"(+Filter)") + optionshtml;
 		selector3.innerHTML = o(0,"(-Filter)") + notoptionshtml;
 		
-		if (l(document.location.hash) !== 0){
-			[selector1.value, selector2.value, selector3.value, filterselector.value, shouldloop] = JSON.parse(document.location.hash.substr(1));
+		if (l(h) !== 0){
+			[selector1.value, selector2.value, selector3.value, filterselector.value, shouldloop] = JSON.parse(h.substr(1));
 			if (shouldloop === 0){
 				media_video.loop = false;
 				//media_audio.loop = false;
